e2d59e5d3a993d9fb7ac74897d74e1ce6f56ee47
Breno Rossi <brenorossiduarte@gmail.com>
2025-09-15 20:37:07 -0300

    Implementação do Classe Vagão
    Todas as classe com as heranças e print implementados NOTA!!!- Excluir a funcao 'main' apos finalizar (implementada para fins de teste)

--- Vagao.py
+++ Vagao.py
@@ -3,35 +3,57 @@ class Vagao:
     def __init__(self, comprimento, peso):
         self._comprimento = comprimento
         self._peso = peso
-        
-    
-    def imprime(self):
-        return (f'Dados do Vagão - Comprimento: {self._comprimento}, Peso: {self._peso}')
 
+    def imprime(self):
+        print(f'--------------Dados do Vagão--------------------')
+        print (f'Comprimento: {self._comprimento}m')
+        print (f'Peso: {self._peso}Tons')
 
 class Locomotiva(Vagao):
-    def  __init__(self, comprimento, peso,potencia):
-        self._potencia = potencia
+    def  __init__(self, comprimento, peso, potencia):
         super().__init__(comprimento, peso)
-
+        self._potencia = potencia
+        
     def imprime(self):
         super().imprime()
-        return (f'Dados da Locomotiva - Potência: {self._potencia}')
+        print(f'Potência: {self._potencia}HP')
    
 class Carga(Vagao):
     def  __init__(self, comprimento, peso,carga):
-        self._carga= carga
         super().__init__(comprimento, peso)
+        self._carga= carga
 
     def imprime(self):
         super().imprime()
-        return (f'Dados do Vagão de Carga - Carga: {self._carga}')
+        print(f'Carga: {self._carga}ton')
 
 class Passageiro(Vagao):
     def  __init__(self, comprimento, peso, capacidade):
-        self._capacidade = capacidade
         super().__init__(comprimento, peso)
-
+        self._capacidade = capacidade
+        
     def imprime(self):
         super().imprime()
-        return (f'Dados do Vagão de Passageiro - Capacidade: {self._capacidade}')
+        print(f'Capacidade: {self._capacidade} ')
+        #print(f'Dados do Vagão de Passageiro - Capacidade: {self._capacidade}')
+
+
+if __name__ == "__main__":
+    
+    locomotiva = Locomotiva(15, 5000, 3000) #comprimento, peso, potencia
+    vagao_carga = Carga(12, 3000, 10000) #comprimento, peso,carga
+    vagao_passageiro = Passageiro(14, 2500, 50) #comprimento, peso, capacidade
+
+    print("=== Teste dos métodos imprime() ===\n")
+    
+    print("Locomotiva:")
+    locomotiva.imprime()
+    print()
+    
+    print("Vagão de Carga:")
+    vagao_carga.imprime()
+    print()
+    
+    print("Vagão de Passageiro:")
+    vagao_passageiro.imprime()
+    print()
